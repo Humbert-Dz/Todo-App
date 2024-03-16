16886abe8e29c63f2245060541ed9232c54d16d2
Humberto Domínguez <humbertodelac13@gmail.com>
2024-03-16 17:31:34 -0600

    crear un nuevo todo

--- src/todos/app.js
+++ src/todos/app.js
@@ -4,7 +4,10 @@ import { renderTodos } from "./use-cases";
 
 //* objeto literal para recopilar ids de elementos segun nos interese
 const elementsIDs = {
+  //cuadro donde se van a meter los todos
   listaTODOS: ".todo-list",
+  //inout donde se ingresa el todo
+  NewTodoInput: "#new-todo-input",
 };
 
 /**
@@ -34,6 +37,31 @@ const App = (elementId) => {
     //llamamos a la funcion que renderiza los todos
     renderizarTodos();
   })();
+
+  //refererencia a input para agregar un
+  const newDescriptionInput = document.querySelector(elementsIDs.NewTodoInput);
+
+  //agregamos un evento al input de agregar todo
+  newDescriptionInput.addEventListener("keyup", (e) => {
+    //si la tecla presionada no es el enter (cada tecla tiene un keyCode, el del enter es 13)
+    if (e.keyCode !== 13) return;
+
+    // si el elemento despues de quitar espacios en blanco al inicio y fin, tiene menos de 5 caracteres
+    //se sale del evento
+    if (e.target.value.trim().length < 5) return;
+
+    //imprime en consola el valor
+    console.log(e.target.value);
+
+    //al store agrega el valor del input, que es la descripcion del todo, (lo que se tiene que hacer)
+    todoStore.addTodo(e.target.value);
+
+    //limpiamos el input para agregar un nuevo todo
+    newDescriptionInput.value = "";
+
+    //renderiza los todos
+    renderizarTodos();
+  });
 };
 
 export default App;
